b155c74c8bf300f59678f477dfd6979b5e8a74e7
Lucas <lukassforce123@gmail.com>
2025-07-31 16:14:47 -0400

    Creacion datos iniciales para base de datos

---
+++ docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,14 @@
+INSERT INTO "Productos" (Id, Nombre, Precio) VALUES
+  (1, 'Teclado', 100),
+  (2, 'Mouse', 50),
+  (3, 'Monitor', 300),
+  (4, 'Notebook', 800),
+  (5, 'Impresora', 200),
+  (6, 'Escáner', 150),
+  (7, 'Webcam', 75),
+  (8, 'Auriculares', 60),
+  (9, 'Disco Duro Externo', 120),
+  (10, 'Pendrive', 30);
+
+INSERT INTO "User" (Id, Username, Password) VALUES
+  (1, 'Lucas', 'AQAAAAIAAYagAAAAEDE9Oywd5hn0dOFGuGfVHkA+DRrGiDJhOmEevDij34hRcWpAYZ9I8IulporLriXUsw==');
